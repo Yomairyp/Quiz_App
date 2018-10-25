8a6a4b14b4eaa850f6566bdaabca9c4bb9b75250
Yomairy Pena <misspena21@gmail.com>
2018-10-25 12:33:13 -0400

    function that check if the questions have ended

--- quiz_controller.js
+++ quiz_controller.js
@@ -1,6 +1,6 @@
 function Quiz (question){
     this.score = 0;
-    this.question = question;
+    this.questions = questions;
     this.questionIndex = 0;
 };
 
@@ -9,3 +9,7 @@ Quiz.prototype.getQuestionIndex = function (){
     return this.question[this.questionIndex]
 
 };
+
+Quiz.prototype.isEnded = function(){
+    return this.length === this.questionIndex
+}
