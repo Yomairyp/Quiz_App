eb8a4bbeeaa219678442615eabfe8e3adaf1773d
Yomairy Pena <misspena21@gmail.com>
2018-10-25 11:44:07 -0400

    added js files, and added 2 functions one for questions thats accepts a text, choice and answer. second one for correct answers.

---
+++ questions.js
@@ -0,0 +1,11 @@
+// constructor function
+function Question (text,choices,answer){
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer
+};
+
+Question.prototype.correctAnswer = function(choice){
+    choice === this.answer;
+
+};
