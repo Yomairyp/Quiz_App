7f5432c7b515f4ee99e1331d6b56ab369aff25d6
Yomairy Pena <misspena21@gmail.com>
2018-10-25 21:22:55 -0400

    Array of quiz questions, with answer choices and answers.

---
+++ app.js
@@ -0,0 +1,6 @@
+let question = [
+    new Questions ("What does MVC stand for?"["Mobile-Version-Connection", "Model-Version-Control","Model-View-Control"],"Model-View-Control"),
+    new Question ("What is the does HTML stand for?"["Hypertext Markup Language", "Hyper Language", "Mark up Language"],"Hypertext Markup Language"),
+    new Question ("Whats language is used for we apps?",["Java", "javaScript", "PHP", "All"], "All"),
+    new Question ("What language is used to style a page?"["HTML","CSS", "Java","Ruby"],"CSS")
+];
