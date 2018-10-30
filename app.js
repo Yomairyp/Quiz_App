ad8573b887529cdfb8ad6f4a99ac2e43d8f0c271
Yomairy Pena <misspena21@gmail.com>
2018-10-29 22:06:48 -0400

    changed class btn to id, function that lets you guess and go to the next question.

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ function populate () {
       for( let i = 0; i < choices.length; i++) { 
           let element = document.getElementById("choice" + i);
           element.innerHTML = choices[i];
-        
+         guess("btn" + [i], choices[i])
     }
-    
-      
-    };
+      showProgress()
+    }
+
+};
+
+function guess(id,guess){
+    let button = document.getElementById(id);
+    button.onclick = function (){
+        quiz.guess(guess);
+        populate()
+    }
 
 };
 
@@ -29,7 +37,7 @@ function showScore(){
 let questions = [
     new Question ("What does MVC stand for?",["Mobile-Version-Connect", "Model-Version-Control","Model-View-Controler", "Mobile-View-Connection"],"Model-View-Control"),
     new Question ("What is the does HTML stand for?",["Hypertext Markup Language", "Hyper Language", "Mark up Language"],"Hypertext Markup Language"),
-    new Question ("Whats language is used for we apps?",["Java", "javaScript", "PHP", "All"], "All"),
+    new Question ("Whats language is used for web apps?",["Java", "javaScript", "PHP", "All"], "All"),
     new Question ("What language is used to style a page?",["HTML","CSS", "Java","Ruby"],"CSS")
 ];
 
