7bea7605c78eae34b31697246d4ac07a2dd6f3aa
Yomairy Pena <misspena21@gmail.com>
2018-11-05 16:11:14 -0500

    Editing

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ function guess(id,guess){
 
 function showScore(){
     let gameOverHTML = "<h1>Results</h1>";
-    gameOverHTML += "<h2 id='score'>Your scores: " + quiz.score + "</h2>";
+    gameOverHTML += "<h2 id='score'>Your score: " + quiz.score + "</h2>";
     let element = document.getElementById("quiz");
     element.innerHTML = gameOverHTML;
 }
 
 let questions = [
-    new Question ("What does MVC stand for?",["Mobile-Version-Connect", "Model-Version-Control","Model-View-Controler", "Mobile-View-Connection"],"Model-View-Control"),
+    new Question ("What does MVC stand for?",["Mobile-Version-Connect", "Model-Version-Control","Model-View-Controller", "Mobile-View-Connection"],"Model-View-Control"),
     new Question ("What is the does HTML stand for?",["Hypertext Markup Language", "Hyper Language", "Mark up Language"],"Hypertext Markup Language"),
     new Question ("Whats language is used for web apps?",["Java", "javaScript", "PHP", "All"], "All"),
     new Question ("What language is used to style a page?",["HTML","CSS", "Java","Ruby"],"CSS")
